65c4378ab1cb5c7aadb81ac246c77d819d56f518
yujie <158392613@qq.com>
2017-06-06 15:40:26 +0800

    2017年6月6日 15:40:15

---
+++ app/src/main/java/com/yujie/yjclock/StopWatch.java
@@ -0,0 +1,25 @@
+package com.yujie.yjclock;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2017/6/6 0006.
+ */
+
+public class StopWatch extends LinearLayout {
+    public StopWatch(Context context) {
+        super(context);
+    }
+
+    public StopWatch(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+}
